373d4b9cac55ef31c51e32206fa22ef6d12ee4db
AditiHB <aditipatil276@gmail.com>
2026-05-02 14:16:40 +0530

    Added logger and increased instances of inventory service

--- services/customer-service/src/main/java/com/ecommerce/customerservice/CustomerController.java
+++ services/customer-service/src/main/java/com/ecommerce/customerservice/CustomerController.java
@@ -3,9 +3,14 @@ package com.ecommerce.customerservice;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @RestController
 @RequestMapping("/api/customers")
 public class CustomerController {
+    
+    private static final Logger log = LoggerFactory.getLogger(CustomerController.class);
     
     private final CustomerRepository repository;
 
@@ -15,11 +20,13 @@ public class CustomerController {
 
     @GetMapping
     public List<Customer> getAll() {
+        log.info("Fetching all customers");
         return repository.findAll();
     }
 
     @PostMapping
     public Customer create(@RequestBody Customer customer) {
+        log.info("Creating a new customer: {}", customer.getName());
         return repository.save(customer);
     }
 }
